b363f2f7883b58808a5e6f58693b1d1c496dd6cf
Andras Findt <anthrax.f@gmail.com>
2020-02-16 14:31:32 +0200

    Attempts at making brew install things for me. :/

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf "./roles/andrasfindt.*"
+rm -rf ./roles/andrasfindt.*
 ansible-galaxy install -r requirements.yml -p ./roles
 ansible-playbook playbook.yml -vvv -u build --extra-vars '@secrets' --vault-password-file=vault.txt -i hosts.yml
