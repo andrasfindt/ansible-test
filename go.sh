6b87a65fce5600ae18dd8a3f4cf0b3ec31ffb1f5
Andras Findt <anthrax.f@gmail.com>
2020-02-16 16:01:44 +0200

    cloning depth 1 of powerlevel10k

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+echo "Deleting andrasfindt.* playbooks"
 rm -rf ./roles/andrasfindt.*
+echo "Installing requirements"
 ansible-galaxy install -r requirements.yml -p ./roles
+echo "Executing playbook"
 ansible-playbook playbook.yml -vv -u build --extra-vars '@secrets' --vault-password-file=vault.txt -i hosts.yml
